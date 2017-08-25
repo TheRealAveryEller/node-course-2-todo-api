fd8cf76cd6f84edc78329b0dfaad4c1fadd5af77
Avery Eller <j.eller1011@gmail.com>
2017-08-25 11:44:55 -0500

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,43 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB Server');
+    }
+
+    console.log('Connected to MongoDB Server!');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({ text: 'eat lunch' })
+    //     .then(
+    //         (result) => {
+    //             console.log(result);
+    //         }
+    //     );
+    
+    // deleteOne
+    // db.collection('Todos').deleteOne({ text: 'eat lunch' })
+    // .then(
+    //     (result) => {
+    //         console.log(result);
+    //     }
+    // );
+    
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({ completed: false })
+    //     .then(
+    //         (result) => {console.log(result);}
+    //     );
+
+    db.collection('Users').deleteMany({ name: 'Avery Eller' })
+        .then(
+            result => console.log(result)
+        );
+    
+    db.collection('Users').findOneAndDelete({ _id: new ObjectID('59a040b27ad96a1e803582fd') })
+        .then(
+            result => console.log(result)
+        );
+    
+    // db.close();
+});
