0da165d9178e2dbb700e54ca955428631e0bed48
Avery Eller <j.eller1011@gmail.com>
2017-08-25 15:07:51 -0500

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,59 +1,35 @@
-var mongoose = require('mongoose');
+/*******************
+ * Library Imports *
+ *******************/
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+/*****************
+ * Local Imports *
+ *****************/
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minLength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save()
+        .then(
+            (doc) => {
+                res.send(doc);
+            },
+            (err) => {
+                res.status(400).send(err);
+            }
+        );
+})
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-// var newTodo = new Todo({
-//     text: ' Edit all of the crap                    '
-// });
-
-// newTodo.save().then(
-//     doc => console.log('Save was a success!', JSON.stringify(doc, undefined, 2)),
-//     // err => console.log('Save failed', err)
-//     err => {
-//         console.log('Error:', err.errors.text.name)
-//         console.log('Description: ', err.errors.text.message)
-//     }
-// );
-
-// User 
-// email - required|trimmed|string|min-length: 1
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minLength: 1,
-        trim: true
-    }
-});
-
-var newUser = new User({
-    // email: 'j.eller1011@gmail.com'
-});
-
-newUser.save()
-    .then(
-        doc => console.log('New User:', JSON.stringify(doc, undefined, 2)),
-        err => {
-            // console.log(err);
-            console.log('Error:', err.message);
-            // console.log('Description: ', err.errors.text.message);
-        }
-    );
